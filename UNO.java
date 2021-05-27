e8f54a33d0c83557e409f801ae0661b7447ff036
BrandonDu <82884213+BrandonDu@users.noreply.github.com>
2021-05-27 09:20:12 -0400

    stuff

--- UNO.java
+++ UNO.java
@@ -1,11 +1,36 @@
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.*;
 
 // graphics class
 
 public class UNO {
 	
 	private Game game;
+	private JFrame frame;
+	private JPanel panel;
+	private JButton twoPLayer;
+	private JButton threePlayer;
 	
 	UNO() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		ImageIcon background = new ImageIcon("Pictures/homescreen.jpeg");
+		
+		panel = new JPanel();
+		
+		JLabel contentPane = new JLabel();
+		contentPane.setIcon(background);
+		contentPane.setLayout(new BorderLayout());
+		frame.setContentPane(contentPane);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		UNO uno = new UNO();
 	}
 }
