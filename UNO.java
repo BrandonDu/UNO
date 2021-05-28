aed376514ab340fc89ce9e74ce451cfca9292c9a
BrandonDu <82884213+BrandonDu@users.noreply.github.com>
2021-05-28 09:09:54 -0400

    stuff

--- UNO.java
+++ UNO.java
@@ -1,35 +1,106 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
-
 import javax.swing.*;
 
-// graphics class
+public class UNO implements MouseListener {
 
-public class UNO {
-	
 	private Game game;
 	private JFrame frame;
-	private JPanel panel;
+	private JPanel homeScreen;
+	private JPanel optionScreen;
 	private JButton twoPLayer;
 	private JButton threePlayer;
-	
+
 	UNO() {
+
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		ImageIcon background = new ImageIcon("Pictures/homescreen.jpeg");
+		background = scaleImage(background, 1.2, 1.2);
+		Image img = background.getImage();
+		frame.setPreferredSize(new Dimension(background.getIconWidth(), background.getIconHeight()));
 		
-		panel = new JPanel();
+		homeScreen = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(img, 0, 0, null);
+			}
+		};
+		frame.setContentPane(homeScreen);
+
+		homeScreen.setLayout(null);
+		homeScreen.addMouseListener(this);
 		
-		JLabel contentPane = new JLabel();
-		contentPane.setIcon(background);
-		contentPane.setLayout(new BorderLayout());
-		frame.setContentPane(contentPane);
+		JLabel clickAnywhereToContinue = new JLabel("Click Anywhere To Continue");
+		clickAnywhereToContinue.setFont(new Font("Tacoma", Font.PLAIN, 28));
+		clickAnywhereToContinue.setForeground(Color.WHITE);
+		
+		Dimension size = clickAnywhereToContinue.getPreferredSize();
+		clickAnywhereToContinue.setBounds(550, 400, size.width, size.height);
+		frame.getContentPane().add(clickAnywhereToContinue);
+
 		frame.pack();
 		frame.setVisible(true);
 	}
+
+	public static ImageIcon scaleImage(ImageIcon image, double xFactor, double yFactor) {
+		AffineTransform scaleTransform = new AffineTransform();
+		scaleTransform.scale(xFactor, yFactor);
+		Image img = image.getImage();
+		Image newImg = img.getScaledInstance((int) (image.getIconWidth() * xFactor),
+				(int) (image.getIconHeight() * yFactor), java.awt.Image.SCALE_SMOOTH);
+		image = new ImageIcon(newImg);
+		return image;
+	}
 	
+	
+	public static JPanel setOptionScreen() {
+		ImageIcon background = new ImageIcon("Pictures/optionScreen.jpg");
+		background = scaleImage(background, 1.2, 1.2);
+		Image img = background.getImage();
+		return null;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(e.getSource()==homeScreen) {
+			
+			optionScreen = new JPanel();
+			
+		}
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 	public static void main(String[] args) {
 		UNO uno = new UNO();
 	}
+
 }
